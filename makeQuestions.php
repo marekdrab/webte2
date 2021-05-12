fece9eed49731619b0e68f0ab06ebc65f808e692
pandr <pandrejko12@gmail.com>
2021-05-12 18:50:44 +0200

    pridanie tretieho typu otazky do DB

--- makeQuestions.php
+++ makeQuestions.php
@@ -2,13 +2,12 @@
 ini_set('display_errors',1);
 ini_set('display_startup_errors',1);
 error_reporting(E_ALL);
-require_once "partials/loginChecker.php";
 
 //Pridanie 6 otazok (3 pre prvy a druhy typ) do DB staci spustit raz
 //Nezabudni prepisat $conn pre vlastnu DB
 
-require_once "inc/Database.php";
-$conn = (new Database())->createConnection();
+require_once "inc/DatabasePeter.php";
+$conn = (new DatabasePeter())->getConnection();
 
 $question1 = "Aké je hlavné mesto Nemecka?";
 $correctAnswer1 = "Berlin";
@@ -37,10 +36,20 @@ $correctAnswer6 = "18";
 $otherAnswers6 = ["21", "17", "19", "20"];
 $type6 = 2;
 
+$question7 = "2+2,4*4,5+8,6+12";
+$correctAnswer7 = "4,16,13,18";
+$type7 = 3;
+
+$question8 = "Slovensko,Rusko,Nemecko,Rakúsko";
+$correctAnswer8 = "Bratislava,Moskva,Berlín,Viedeň";
+$type8 = 3;
+
 //vlozi otazku prveho typu - kratka odpoved
 $stmQuestion1 = $conn->prepare("INSERT INTO questions(question, type_id, correct_answer_id) VALUES(?,?,?)");
 //vlozi otazku druheho typu - moznosti
 $stmQuestion2 = $conn->prepare("INSERT INTO questions(question, type_id, correct_answer_id, all_answers_id) VALUES(?,?,?,?)");
+//vlozi otazku tretieho typu - parovanie
+$stmQuestion3 = $conn->prepare("INSERT INTO questions(question, type_id, correct_answer_id) VALUES(?,?,?)");
 //vlozi odpovede do tabulky
 $stmAnswer = $conn->prepare("INSERT INTO answers(answer, is_correct) VALUES(?,?)");
 //ziska ID odpovede
@@ -53,6 +62,8 @@ $stmAnswer->execute([$correctAnswer3, 1]);
 $stmAnswer->execute([$correctAnswer4, 1]);
 $stmAnswer->execute([$correctAnswer5, 1]);
 $stmAnswer->execute([$correctAnswer6, 1]);
+$stmAnswer->execute([$correctAnswer7, 1]);
+$stmAnswer->execute([$correctAnswer8, 1]);
 
 //ziskanie id ostatnych odpovedi - vo fore sa dopisuju do tychto stringov
 $otherAnswersID4 = "";
@@ -103,12 +114,21 @@ $correctAnswerID5 = $stmGetAnswerID->fetch(PDO::FETCH_ASSOC);
 $stmGetAnswerID->execute([$correctAnswer6]);
 $correctAnswerID6 = $stmGetAnswerID->fetch(PDO::FETCH_ASSOC);
 
+$stmGetAnswerID->execute([$correctAnswer7]);
+$correctAnswerID7 = $stmGetAnswerID->fetch(PDO::FETCH_ASSOC);
+
+$stmGetAnswerID->execute([$correctAnswer8]);
+$correctAnswerID8 = $stmGetAnswerID->fetch(PDO::FETCH_ASSOC);
 
 //vlozenie otazok do tabulky - prvy a druhy typ
 $stmQuestion1->execute([$question1, $type1, $correctAnswerID1['id']]);
 $stmQuestion1->execute([$question2, $type2, $correctAnswerID2['id']]);
 $stmQuestion1->execute([$question3, $type3, $correctAnswerID3['id']]);
+
 $stmQuestion2->execute([$question4, $type4, $correctAnswerID4['id'], $otherAnswersID4]);
 $stmQuestion2->execute([$question5, $type5, $correctAnswerID5['id'], $otherAnswersID5]);
 $stmQuestion2->execute([$question6, $type6, $correctAnswerID6['id'], $otherAnswersID6]);
+
+$stmQuestion3->execute([$question7, $type7, $correctAnswerID7['id']]);
+$stmQuestion3->execute([$question8, $type8, $correctAnswerID8['id']]);
 
