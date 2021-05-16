4043304504068920246e51135e0bef59badc07ea
Marek <marek.drab@itaps.sk>
2021-05-16 16:34:57 +0200

    pridanie suboru pre dokumentaciu

--- domov.php
+++ domov.php
@@ -9,6 +9,7 @@ echo getHeaderTeacher($_SESSION['name'], $_SESSION['surname'], $_SESSION["loginT
         <button onclick="window.location.href='tests.php'" class="btn" id="testInfo" type="button">Všetky testy</button>
         <button onclick="window.location.href='passTests.php'" class="btn" id="testInfo" type="button">Odovzdané testy</button>
         <button onclick="window.location.href='outNotification.php'" class="btn" id="testInfo" type="button">Prebiehajúce testy</button>
+        <button onclick="window.location.href='dokumentacia.php'" class="btn" id="testInfo" type="button">Dokumentácia</button>
     </div>
 
 </div>
