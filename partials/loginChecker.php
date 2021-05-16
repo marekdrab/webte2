b13c293382a842cc104c41a0ce6b40096376fa8a
pandr <pandrejko12@gmail.com>
2021-05-16 21:58:13 +0200

    fix pristup studenta na domov.php alebo ostatne pre ucitelov

--- partials/loginChecker.php
+++ partials/loginChecker.php
@@ -1,9 +1,10 @@
 <?php
 
 session_start();
- if((empty($_SESSION['name']) || empty($_SESSION['surname']) )&& ($_SESSION['loginType'] == "Študent")) {
-    header("location: https://wt41.fei.stuba.sk/final/index.php");
-    session_destroy();
- }    
+if (isset($_SESSION['loginType'])) {
+    if ((empty($_SESSION['name']) || empty($_SESSION['surname'])) && ($_SESSION['loginType'] == "Študent")) {
+        header("location: https://wt41.fei.stuba.sk/final/index.php");
+    }
+}
 
 
