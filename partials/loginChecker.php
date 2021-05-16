d4e7aeeb05a26074e0a178ea6429957435cb74e5
Marek <marek.drab@itaps.sk>
2021-05-16 19:51:14 +0200

    fix loginChecker.php

--- partials/loginChecker.php
+++ partials/loginChecker.php
@@ -1,7 +1,7 @@
 <?php
 
 session_start();
- if(empty($_SESSION['name']) || empty($_SESSION['surname']) || ($_SESSION['loginType'] == "Študent")) {
+ if((empty($_SESSION['name']) || empty($_SESSION['surname']) )&& ($_SESSION['loginType'] == "Študent")) {
     header("location: https://wt41.fei.stuba.sk/final/index.php");
     session_destroy();
  }    
