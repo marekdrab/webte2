9e5e9c21674923f3ce62397ab4b0e3f5a34a811e
Marek <marek.drab@itaps.sk>
2021-05-16 22:31:11 +0200

    pridanie dokumentacie

--- dokumentacia.php
+++ dokumentacia.php
@@ -5,18 +5,100 @@ require_once "partials/header.php";
 echo getHead('Dokumentácia');
 echo getHeaderTeacher($_SESSION['name'], $_SESSION['surname'], $_SESSION["loginType"]); ?>
 <div class="container documentation">
-    <h3>Dokumentácia</h3>
-    <p>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece
-        of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock,
-        a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure
-        Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word
-        in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections
-        1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by
-        Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during
-        the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.
-        The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested.
-        Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced
-        in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.</p>
+    <div class="row">
+        <div class="col-md-12">
+            <h1>Dokumentácia</h1>
+            <h4>Párovacie otázky</h4>
+            <p>jsPlumb</p>
+            <h4>Otázky s kreslením</h4>
+            <p>drawingboard.js</p>
+            <h4>Matematické výrazy</h4>
+            <p>mathquill.js</p>
+            <h4>PDF export</h4>
+            <a href="http://www.fpdf.org/">FPDF</a>
+            <h4>PHPGangsta</h4>
+            <p>Prihlasovanie</p>
+        </div>
+    </div>
+    <hr>
+    <div class="row">
+        <div class="col-md-12">
+            <h1>Rozdelenie úloh</h1>
+            <table class="table">
+                <tbody>
+                <tr>
+                    <td>Prihlasovanie</td>
+                    <td>Dávid Gavenda</td>
+                </tr>
+                <tr>
+                    <td>Otázky s krátkymi odpoveďami (zobrazenie, automatické vyhodnotenie)</td>
+                    <td>Peter Andrejko</td>
+                </tr>
+                <tr>
+                    <td>Otázky s možnosťami (zobrazenie, automatické vyhodnotenie)</td>
+                    <td>Peter Andrejko</td>
+                </tr>
+                <tr>
+                    <td>Párovacie otázky (zobrazenie, automatické vyhodnotenie)</td>
+                    <td>Peter Andrejko</td>
+                </tr>
+                <tr>
+                    <td>Otázky s kreslením (zobrazenie, vkladanie výsledku do testu)</td>
+                    <td>Tomáš Danko</td>
+                </tr>
+                <tr>
+                    <td>Otázky s matematickým výrazom (zobrazenie, vkladanie výsledku do testu)</td>
+                    <td>Tomáš Danko</td>
+                </tr>
+                <tr>
+                    <td>Zadávanie otázok do testu</td>
+                    <td>Marek Dráb</td>
+                </tr>
+                <tr>
+                    <td>Pridávanie viacerých testov, aktivácia a deaktivácia testov</td>
+                    <td>Marek Dráb</td>
+                </tr>
+                <tr>
+                    <td>Manuálne vyhodnotenie testu učiteľom</td>
+                    <td>Marek Dráb</td>
+                </tr>
+                <tr>
+                    <td>Info o zbiehaní testov</td>
+                    <td>Michal Hamrák</td>
+                </tr>
+                <tr>
+                    <td>Uloženie a vyhodnotenie testu</td>
+                    <td>Peter Andrejko</td>
+                </tr>
+                <tr>
+                    <td>Časovač a ukončenie testu po uplynutí času</td>
+                    <td>Dávid Gavenda</td>
+                </tr>
+                <tr>
+                    <td>Export do PDF</td>
+                    <td>Marek Dráb, Michal Hamrák</td>
+                </tr>
+                <tr>
+                    <td>Export do CSV</td>
+                    <td>Peter Andrejko</td>
+                </tr>
+                <tr>
+                    <td>Docker balíček</td>
+                    <td>Dávid Gavenda</td>
+                </tr>
+                <tr>
+                    <td>Dizajn a štruktúra aplikácie</td>
+                    <td>Michal Hamrák</td>
+                </tr>
+                <tr>
+                    <td>Databáza</td>
+                    <td>Marek Dráb</td>
+                </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+<div class="space"></div>
 </div>
 
 <?php echo getFooter();?>
