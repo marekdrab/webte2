9efadcfe3f1f6decda622caae64855fb0aca112a
pandr <pandrejko12@gmail.com>
2021-05-16 22:04:37 +0200

    fix loginChecker.php

--- partials/loginChecker.php
+++ partials/loginChecker.php
@@ -1,8 +1,13 @@
 <?php
 
 session_start();
-if((empty($_SESSION['name']) || empty($_SESSION['surname']) || empty($_SESSION['loginType'])) && ($_SESSION['loginType'] == "Študent")) {
+if (!isset($_SESSION['loginType'])){
     header("location: https://wt41.fei.stuba.sk/final/index.php");
+}
+else {
+    if ((empty($_SESSION['name']) || empty($_SESSION['surname'])) && ($_SESSION['loginType'] == "Študent")) {
+        header("location: https://wt41.fei.stuba.sk/final/index.php");
+    }
 }
 
 
